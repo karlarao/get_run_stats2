a91b099f7e05c06b2c34fa7eaf52497517ce4449
karlarao <karlarao@gmail.com>
2018-07-12 15:30:25 -0400

    args check

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# args check
+usage() { 
+    echo "Usage: $0 <baselineperf|gluentperf>" 1>&2; exit 1; 
+}
+ 
+t=$1
+if [[ -z ${t} ]]; then
+    usage
+elif ! [[ ${t} == "baselineperf" || ${t} == "gluentperf" ]]; then
+    usage
+fi 
+ 
 
 # get all sql files to execute 
 FILELIST=`ls -1 | grep .sql | grep -v run | grep -v planx | grep -v grs | grep -v sqlmon`
@@ -15,7 +27,7 @@ do
         @$i    
         COL p_sqlid NEW_V p_sqlid;
         select prev_sql_id p_sqlid from v\$session where sid=sys_context('userenv','sid');                                                                   
-        exec get_snap_time.end_snap('$i','$1','&p_sqlid')    
+        exec get_snap_time.end_snap('$i','$t','&p_sqlid')    
         -- @sqlmon.sql userenv('sid')
                                            
 EOF
